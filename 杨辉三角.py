64a42ea0d3d35a3d2de9d72f9ed9d12fa8b77b39
yanchidezhang <yanchidezhang@163.com>
2020-02-26 21:56:45 +0800

    Update 杨辉三角.py

--- 杨辉三角.py
+++ 杨辉三角.py
@@ -8,20 +8,23 @@ from time import time
 # f(i,j) = 1 where j = 1 or i=j
 targetRow = 5
 
-
-# def triangle(n):
-#     if n == 0:
-#         return [1]
-#     else:
-#         return [([0] + row(n - 1))[index] + (row(n - 1) + [0])[index] for index in range(n + 1)]
+# Approach 1
 
 
-# start = time()
-# print(row(targetRow))
-# end = time() # 0.03157806396484375
-# print(end - start)
+def triangle(n):
+    if n == 0:
+        return [1]
+    else:
+        return [([0] + row(n - 1))[index] + (row(n - 1) + [0])[index] for index in range(n + 1)]
 
 
+start = time()
+print(row(targetRow))
+end = time()  # 0.03157806396484375
+print(end - start)
+
+
+# Approach 2
 def triangle(row):
     a = [1]
 
@@ -38,5 +41,6 @@ for row in rows:
     counter += 1
     if counter == targetRow + 1:
         print(row)
+
 end = time()  # 0.00008797645568847656
 print(end - start)
